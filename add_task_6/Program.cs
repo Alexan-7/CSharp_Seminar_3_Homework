f6459222c0db5e7644253c7a04abcba7560baba0
Sasha <a40546b@gmail.com>
2022-09-09 19:37:33 +0300

    first commit

---
+++ add_task_6/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит координаты двух точек A и B на плоскости. 
+// Программа определяет, какая из них дальше от начала координат.
+
+using System;
+
+Console.WriteLine("Введите координаты первой точки (X Y) через пробел: ");
+String xy = Console.ReadLine();
+string[] parts = xy.Split(' ');
+int xA = int.Parse(parts[0]);
+int yA = int.Parse(parts[1]);
+
+Console.WriteLine("Введите координаты второй точки (X Y) через пробел: ");
+xy = Console.ReadLine();
+parts = xy.Split(' ');
+int xB = int.Parse(parts[0]);
+int yB = int.Parse(parts[1]);
+
+double Distance1 = Math.Round(Math.Sqrt(Math.Pow(xA, 2) + Math.Pow(yA, 2)), 3, MidpointRounding.AwayFromZero);
+double Distance2 = Math.Round(Math.Sqrt(Math.Pow(xB, 2) + Math.Pow(yB, 2)), 3, MidpointRounding.AwayFromZero);
+Console.WriteLine($"Расстояние точек до О(0; 0): от 1 = {Distance1}, от 2 = {Distance2}");
+
+if (Distance1 == Distance2)
+{
+    Console.WriteLine("Точки находятся на одинаковом расстоянии от О(0; 0)");
+}
+else if (Distance1 < Distance2)
+{
+    Console.WriteLine("От начала координат дальше расположена вторая точка");
+}
+else 
+{
+    Console.WriteLine("От начала координат дальше расположена первая точка");
+}
---
+++ hw_21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+using System;
+
+Console.WriteLine("Введите три координаты первой точки (X Y Z) через пробел: ");
+String xyz = Console.ReadLine();
+string[] parts = xyz.Split(' ');
+int xA = int.Parse(parts[0]);
+int yA = int.Parse(parts[1]);
+int zA = int.Parse(parts[2]);
+
+Console.WriteLine("Введите три координаты второй точки (X Y Z) через пробел: ");
+xyz = Console.ReadLine();
+parts = xyz.Split(' ');
+int xB = int.Parse(parts[0]);
+int yB = int.Parse(parts[1]);
+int zB = int.Parse(parts[2]);
+
+// диагональ прямоугольного параллелепипеда
+double Distance = Math.Sqrt(Math.Pow((xA - xB), 2) + Math.Pow((yA - yB), 2) + Math.Pow((zA - zB), 2));
+
+Console.WriteLine("Расстояние между этими точками в 3D-пространстве = " + Distance);
